161d79b825f172de59684eda7baa45d072c0ce78
Mahmoud Galal <ssss.aaa2002@gmail.com>
2024-09-08 21:29:36 +0300

    optimize the build script

--- scripts/build_plugin.sh
+++ scripts/build_plugin.sh
@@ -3,22 +3,24 @@
 set -e
 
 # Define variables
-PLUGIN_NAME="obb-key-management-plugin"
-PLUGIN_SOURCE_FILE="obb-key-management-plugin.c"
+PLUGIN_NAME="obb-key-plugin"
+PLUGIN_SOURCE_FILE="mariadb_plugin.c"
 PLUGIN_OUTPUT_FILE="${PLUGIN_NAME}.so"
-MARIADB_INCLUDE_PATH="/usr/include/mysql"
-MARIADB_LIB_PATH="/usr/lib/mysql"
+MARIADB_INCLUDE_PATH="/usr/mysql/include"
+MARIADB_LIB_PATH="/usr/mysql/lib"
 
 # Install necessary dependencies
 echo "Installing dependencies..."
 sudo apt-get update
-sudo apt-get install -y libmariadb-dev libcurl4-openssl-dev gcc make
+sudo apt-get install -y libmariadb-dev libcurl4-openssl-dev gcc make libmysqlclient-dev
 
-find /usr/include -name plugin.h
+echo "Compiling the Obb Service lib..."
+gcc -c obb_key_service.c -o obb_key_service.o -lcurl
+ar rcs libobb_key_service.a obb_key_service.o
 
 # Compile the plugin
 echo "Compiling the MariaDB plugin..."
-gcc -fPIC -shared -o $PLUGIN_OUTPUT_FILE $PLUGIN_SOURCE_FILE -I$MARIADB_INCLUDE_PATH -L$MARIADB_LIB_PATH -lmysqlclient -lcurl
+gcc -fPIC -shared -o -lobb_key_service $PLUGIN_OUTPUT_FILE $PLUGIN_SOURCE_FILE -I$MARIADB_INCLUDE_PATH -L$MARIADB_LIB_PATH -lmysqlclient -lcurl
 
 # Output result
 if [ -f "$PLUGIN_OUTPUT_FILE" ]; then
