db9d0adbc0b4516e7a76e3efa2eda22f88fbfb16
Mahmoud Galal <ssss.aaa2002@gmail.com>
2024-09-20 19:57:12 +0300

    latest version with some fixes

--- onboardbase_key_management_plugin.cc
+++ onboardbase_key_management_plugin.cc
@@ -17,6 +17,22 @@
 #define MAX_RESPONSE_SIZE 131072
 #define MAX_URL_SIZE 32768
 
+typedef struct KEY_INFO
+{
+  unsigned int key_id;
+  unsigned int key_version;
+  clock_t timestamp;
+  unsigned int length;
+  unsigned char data [MY_AES_MAX_KEY_LENGTH];
+  KEY_INFO() : key_id(0), key_version(0), timestamp(0), length(0) {};
+  KEY_INFO(unsigned int key_id_,
+           unsigned int key_version_,
+           clock_t timestamp_,
+           unsigned int length_) :
+    key_id(key_id_), key_version(key_version_),
+    timestamp(timestamp_), length(length_) {};
+} KEY_INFO;
+
 class OBData
 {
 private:
@@ -28,16 +44,59 @@ private:
     bool curl_inited;
 
 public:
-    OBData();
-    ~OBData();
+    OBData()
+     :slist(NULL),
+     vault_url_data(NULL),
+     vault_url_len(0),
+     local_token(NULL),
+     token_header(NULL),
+     curl_inited(false)
+   {}
     unsigned int get_key_from_vault(unsigned int version, unsigned int key_id, unsigned char *dstbuf, unsigned int *buflen);
+    unsigned int get_latest_version();
     int init();
-    void deinit();
+    void deinit()
+    {
+    if (slist)
+    {
+      curl_slist_free_all(slist);
+      slist = NULL;
+    }
+    if (curl_inited)
+    {
+      curl_global_cleanup();
+      curl_inited = false;
+    }
+    vault_url_len = 0;
+    if (vault_url_data)
+    {
+      free(vault_url_data);
+      vault_url_data = NULL;
+    }
+    if (token_header)
+    {
+      free(token_header);
+      token_header = NULL;
+    }
+    if (local_token)
+    {
+      free(local_token);
+      local_token = NULL;
+    }
+  }
 
 private:
     int curl_run(const char *url, std::string *response) const;
-    void *alloc(size_t nbytes) const;
-    static size_t write_response_memory(void *contents, size_t size, size_t nmemb, void *userp);
+    void *alloc (size_t nbytes) const
+    {
+    void *res = (char *) malloc(nbytes);
+    if (!res)
+    {
+      my_printf_error(ER_UNKNOWN_ERROR, PLUGIN_ERROR_HEADER
+                      "Memory allocation error", 0);
+    }
+    return res;
+    }
     static std::string extract_value(const std::string &json_str, const std::string &key);
 };
 
@@ -77,103 +136,117 @@ static struct st_mysql_sys_var *settings[] = {
     NULL
 };
 
-OBData::OBData()
-    : slist(NULL), vault_url_data(NULL), vault_url_len(0),
-      local_token(NULL), token_header(NULL), curl_inited(false) {}
 
-OBData::~OBData() {
-    deinit();
-}
-
-void OBData::deinit() {
-    if (slist) {
-        curl_slist_free_all(slist);
-        slist = NULL;
-    }
-    if (curl_inited) {
-        curl_global_cleanup();
-        curl_inited = false;
-    }
-    free(vault_url_data);
-    free(token_header);
-    free(local_token);
-    vault_url_data = NULL;
-    token_header = NULL;
-    local_token = NULL;
-    vault_url_len = 0;
-}
-
-void *OBData::alloc(size_t nbytes) const {
-    void *res = malloc(nbytes);
-    if (!res) {
-        my_printf_error(ER_UNKNOWN_ERROR, PLUGIN_ERROR_HEADER
-                        "Memory allocation error", 0);
-    }
-    return res;
-}
-
-size_t OBData::write_response_memory(void *contents, size_t size, size_t nmemb, void *userp) {
-    size_t realsize = size * nmemb;
-    std::ostringstream *read_data = static_cast<std::ostringstream *>(userp);
-    size_t current_length = read_data->tellp();
-    if (current_length + realsize > MAX_RESPONSE_SIZE)
-        return 0; // response size limit exceeded
-    read_data->write(static_cast<char *>(contents), realsize);
-    return read_data->good() ? realsize : 0;
-}
-
-int OBData::curl_run(const char *url, std::string *response) const {
-    CURL *curl = curl_easy_init();
-    if (!curl) {
-        my_printf_error(ER_UNKNOWN_ERROR, PLUGIN_ERROR_HEADER
-                        "Cannot initialize curl session", ME_ERROR_LOG_ONLY);
-        return 1;
-    }
-
-    std::ostringstream read_data_stream;
-    char curl_errbuf[CURL_ERROR_SIZE] = {0};
-
-    curl_easy_setopt(curl, CURLOPT_URL, url);
-    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_response_memory);
-    curl_easy_setopt(curl, CURLOPT_WRITEDATA, &read_data_stream);
-    curl_easy_setopt(curl, CURLOPT_ERRORBUFFER, curl_errbuf);
-    curl_easy_setopt(curl, CURLOPT_HTTPHEADER, slist);
-    curl_easy_setopt(curl, CURLOPT_TIMEOUT, timeout);
-    curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 1L);
-    curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 2L);
-
-    CURLcode res;
-    int retries = max_retries;
-    do {
-        res = curl_easy_perform(curl);
-        if (res != CURLE_OPERATION_TIMEDOUT) break;
-        read_data_stream.str("");
-        read_data_stream.clear();
-    } while (--retries >= 0);
-
-    long http_code = 0;
-    curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &http_code);
-    curl_easy_cleanup(curl);
-
-    if (res != CURLE_OK) {
-        my_printf_error(ER_UNKNOWN_ERROR, PLUGIN_ERROR_HEADER
-                        "curl error: %s", ME_ERROR_LOG_ONLY, curl_errbuf[0] ? curl_errbuf : curl_easy_strerror(res));
-        return 1;
-    }
-
-    if (http_code < 200 || http_code >= 300) {
-        if (http_code != 404) {
-            my_printf_error(ER_UNKNOWN_ERROR, PLUGIN_ERROR_HEADER
-                            "HTTP error: %ld, response: %s", ME_ERROR_LOG_ONLY | ME_WARNING,
-                            http_code, read_data_stream.str().c_str());
-            return 1;
-        }
-        read_data_stream.str("");
-    }
-
-    *response = read_data_stream.str();
+static size_t write_response_memory (void *contents, size_t size, size_t nmemb,
+                                     void *userp)
+{
+  size_t realsize = size * nmemb;
+  std::ostringstream *read_data = static_cast<std::ostringstream *>(userp);
+  size_t current_length = read_data->tellp();
+  if (current_length + realsize > MAX_RESPONSE_SIZE)
+    return 0; // response size limit exceeded
+  read_data->write(static_cast<char *>(contents), realsize);
+  if (!read_data->good())
     return 0;
+  return realsize;
 }
+
+enum {
+   OPERATION_OK,
+   OPERATION_TIMEOUT,
+   OPERATION_ERROR
+};
+
+static CURLcode
+  perform_with_retries (CURL *curl, std::ostringstream *read_data_stream)
+{
+  int retries= max_retries;
+  CURLcode curl_res;
+  do {
+    curl_res= curl_easy_perform(curl);
+    if (curl_res != CURLE_OPERATION_TIMEDOUT)
+    {
+      break;
+    }
+    read_data_stream->clear();
+    read_data_stream->str("");
+  } while (retries--);
+  return curl_res;
+}
+
+
+int OBData::curl_run (const char *url, std::string *response) const
+{
+  char curl_errbuf[CURL_ERROR_SIZE];
+  std::ostringstream read_data_stream;
+  long http_code = 0;
+  CURLcode curl_res = CURLE_OK;
+  CURL *curl = curl_easy_init();
+  if (curl == NULL)
+  {
+    my_printf_error(ER_UNKNOWN_ERROR, PLUGIN_ERROR_HEADER
+                    "Cannot initialize curl session",
+                    ME_ERROR_LOG_ONLY);
+    return OPERATION_ERROR;
+  }
+  curl_errbuf[0] = '\0';
+  if ((curl_res= curl_easy_setopt(curl, CURLOPT_ERRORBUFFER, curl_errbuf)) !=
+          CURLE_OK ||
+      (curl_res= curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION,
+                                  write_response_memory)) != CURLE_OK ||
+      (curl_res= curl_easy_setopt(curl, CURLOPT_WRITEDATA,
+                                  &read_data_stream)) !=
+          CURLE_OK ||
+      (curl_res= curl_easy_setopt(curl, CURLOPT_HTTPHEADER, slist)) !=
+          CURLE_OK ||
+      (curl_res= curl_easy_setopt(curl, CURLOPT_USE_SSL, CURLUSESSL_ALL)) !=
+          CURLE_OK ||
+      (curl_res= curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L)) !=
+          CURLE_OK ||
+      (timeout &&
+       ((curl_res= curl_easy_setopt(curl, CURLOPT_CONNECTTIMEOUT, timeout)) !=
+            CURLE_OK ||
+        (curl_res= curl_easy_setopt(curl, CURLOPT_TIMEOUT, timeout)) !=
+            CURLE_OK)) ||
+      (curl_res = curl_easy_setopt(curl, CURLOPT_URL, url)) != CURLE_OK ||
+      (curl_res = perform_with_retries(curl, &read_data_stream)) != CURLE_OK ||
+      (curl_res = curl_easy_getinfo (curl, CURLINFO_RESPONSE_CODE,
+                                     &http_code)) != CURLE_OK)
+  {
+    curl_easy_cleanup(curl);
+    my_printf_error(ER_UNKNOWN_ERROR, PLUGIN_ERROR_HEADER
+                    "curl returned this error code: %u "
+                    "with the following error message: %s", 0, curl_res,
+                    curl_errbuf[0] ? curl_errbuf :
+                                     curl_easy_strerror(curl_res));
+    return OPERATION_ERROR;
+  }
+  curl_easy_cleanup(curl);
+  *response = read_data_stream.str();
+  bool is_error = http_code < 200 || http_code >= 300;
+  if (is_error)
+  {
+    const char *res = response->c_str();
+    /*
+      Error 404 requires special handling - we should ignore this
+      error at this level, since this means the missing key (this
+      problem is handled at a higher level)
+    */
+    if (http_code == 404)
+    {
+      *response = std::string("");
+      is_error = false;
+    }
+    else if (is_error)
+    {
+      my_printf_error(ER_UNKNOWN_ERROR, PLUGIN_ERROR_HEADER
+                      "Onboardbase server error: %d, response: %s",
+                      ME_ERROR_LOG_ONLY | ME_WARNING, http_code, res);
+    }
+  }
+  return is_error ? OPERATION_ERROR : OPERATION_OK;
+}
+
 
 std::string OBData::extract_value(const std::string &json_str, const std::string &key) {
     std::string search_key = "\"" + key + "\":\"";
@@ -185,17 +258,18 @@ std::string OBData::extract_value(const std::string &json_str, const std::string
     return json_str.substr(start, end - start);
 }
 
+unsigned int OBData::get_latest_version()
+{
+  return 1;
+}
+
+
 unsigned int OBData::get_key_from_vault(unsigned int version, unsigned int key_id, unsigned char *dstbuf, unsigned int *buflen) {
-    // The implementation remains largely the same, but we'll add a version check
-    if (version != 1) {
-        // Handle unsupported version
-        return ENCRYPTION_KEY_VERSION_INVALID;
-    }
 
-    std::string url = std::string(vault_url_data) + std::to_string(key_id);
+    std::string url = std::string(vault_url_data);
     std::string response;
-
-    if (curl_run(url.c_str(), &response) != 0) {
+    
+    if (curl_run(url.c_str(), &response) != OPERATION_OK) {
         my_printf_error(ER_UNKNOWN_ERROR, PLUGIN_ERROR_HEADER
                         "Unable to get key data", 0);
         return ENCRYPTION_KEY_VERSION_INVALID;
@@ -216,26 +290,37 @@ unsigned int OBData::get_key_from_vault(unsigned int version, unsigned int key_i
     }
 
     *buflen = key_value.length();
-    memcpy(dstbuf, key_value.c_str(), *buflen);
+    KEY_INFO info(1, 1, clock(), *buflen);
+    memcpy(info.data, dstbuf, *buflen);
     return 0;
 }
 
+
+static unsigned int get_key_from_vault(unsigned int version, unsigned int key_id, unsigned char *dstbuf, unsigned int *buflen) {
+    return data.get_key_from_vault(version, key_id, dstbuf, buflen);
+}
+
+unsigned int get_latest_version(unsigned int key_id)
+{
+    return data.get_latest_version();
+}
+
+struct st_mariadb_encryption onboardbase_key_management_plugin = {
+    MariaDB_ENCRYPTION_INTERFACE_VERSION,
+    get_latest_version,
+    get_key_from_vault,
+    0, 0, 0, 0, 0
+};
+
 int OBData::init() {
-    my_printf_error(ER_UNKNOWN_ERROR, PLUGIN_ERROR_HEADER "Starting init function", ME_ERROR_LOG_ONLY);
 
     const char *x_vault_token = "store_key:";
     size_t token_len = strlen(token);
 
-    my_printf_error(ER_UNKNOWN_ERROR, PLUGIN_ERROR_HEADER "Checking token length", ME_ERROR_LOG_ONLY);
-
     if (token_len == 0) {
-        my_printf_error(ER_UNKNOWN_ERROR, PLUGIN_ERROR_HEADER "Token is empty, checking VAULT_TOKEN environment variable", ME_ERROR_LOG_ONLY);
-
         char *token_env = getenv("VAULT_TOKEN");
         if (token_env) {
             token_len = strlen(token_env);
-            my_printf_error(ER_UNKNOWN_ERROR, PLUGIN_ERROR_HEADER "Found VAULT_TOKEN in environment", ME_ERROR_LOG_ONLY);
-            
             if (token_len != 0) {
                 local_token = strdup(token_env);
                 if (!local_token) return 1;
@@ -249,21 +334,17 @@ int OBData::init() {
         }
     }
 
-    my_printf_error(ER_UNKNOWN_ERROR, PLUGIN_ERROR_HEADER "Constructing token header", ME_ERROR_LOG_ONLY);
-
+    // Allocate and construct token header
     size_t buf_len = strlen(x_vault_token) + token_len + 1;
     token_header = (char *)alloc(buf_len);
     if (!token_header) return 1;
     snprintf(token_header, buf_len, "%s%s", x_vault_token, token);
 
-    my_printf_error(ER_UNKNOWN_ERROR, PLUGIN_ERROR_HEADER "Token header constructed successfully", ME_ERROR_LOG_ONLY);
-
+    // Allocate vault_url_data
     vault_url_len = strlen(vault_url);
     vault_url_data = (char *)alloc(vault_url_len + 1);
     if (!vault_url_data) return 1;
     snprintf(vault_url_data, vault_url_len + 1, "%s", vault_url);
-
-    my_printf_error(ER_UNKNOWN_ERROR, PLUGIN_ERROR_HEADER "Vault URL prepared", ME_ERROR_LOG_ONLY);
 
     if (curl_global_init(CURL_GLOBAL_ALL) != CURLE_OK) {
         my_printf_error(ER_UNKNOWN_ERROR, PLUGIN_ERROR_HEADER "Unable to initialize curl library", 0);
@@ -271,32 +352,23 @@ int OBData::init() {
     }
     curl_inited = true;
 
-    my_printf_error(ER_UNKNOWN_ERROR, PLUGIN_ERROR_HEADER "cURL library initialized", ME_ERROR_LOG_ONLY);
-
+    // Construct slist with token_header
     slist = curl_slist_append(NULL, token_header);
     if (!slist) {
         my_printf_error(ER_UNKNOWN_ERROR, PLUGIN_ERROR_HEADER "curl: unable to construct slist", 0);
         return 1;
     }
 
-    my_printf_error(ER_UNKNOWN_ERROR, PLUGIN_ERROR_HEADER "Header list constructed successfully", ME_ERROR_LOG_ONLY);
-
-    my_printf_error(ER_UNKNOWN_ERROR, PLUGIN_ERROR_HEADER "Init function completed", ME_ERROR_LOG_ONLY);
     return 0;
 }
-static unsigned int get_key_from_vault(unsigned int version, unsigned int key_id, unsigned char *dstbuf, unsigned int *buflen) {
-    return data.get_key_from_vault(version, key_id, dstbuf, buflen);
-}
-
-struct st_mariadb_encryption onboardbase_key_management_plugin = {
-    MariaDB_ENCRYPTION_INTERFACE_VERSION,
-    NULL, // get_latest_version is not implemented
-    get_key_from_vault,
-    NULL, NULL, NULL, NULL, NULL
-};
 
 static int onboardbase_key_management_plugin_init(void *p) {
-    return data.init();
+  int rc = data.init();
+  if (rc)
+  {
+    data.deinit();
+  }
+  return rc;
 }
 
 static int onboardbase_key_management_plugin_deinit(void *p) {
